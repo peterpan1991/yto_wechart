855b4a67ed8d42f134b4d668c0462d496d6b3286
peterpan1991 <pl404944148@qq.com>
2024-11-24 19:49:34 +0800

    wechat_yto_chart

--- ytochart-backup.py
+++ ytochart-backup.py
@@ -16,13 +16,13 @@ wx.SwitchToThisWindow() #ListControl()方法用于列出所有子级窗口，而
 #寻找会话控件绑定
 hw=wx.ListControl(Name='会话')
 #通过pd读取数据
-df=pd.read_csv('回复数据.csv', encoding='utf-8')
+df=pd.read_csv('./yto_wechart/回复数据.csv', encoding='utf-8')
 print(df)
 
-driver = webdriver.Chrome()
-#打开圆通客服
-driver.get("https://online.yto.net.cn/#/")
-time.sleep(1)
+# driver = webdriver.Chrome()
+# #打开圆通客服
+# driver.get("https://online.yto.net.cn/#/")
+# time.sleep(1)
 
 while True:
     # 从查找未读消息
@@ -36,34 +36,39 @@ while True:
     if we.Name:
         # 点击未读消息
         we.Click(simulateMove=False)
+
+        wx.TextControl(SubName="yto-test").RightClick()
+        exit()
+
         # 读取最后一条消息
         last_msg = wx.ListControl(Name='消息').GetChildren()[-1].Name
 
-        input_element = driver.find_element(By.CLASS_NAME, 'textarea')
-        if(input_element):
-            init_msgs = driver.find_elements(By.CLASS_NAME, "top-center")
-            input_element.send_keys(last_msg, Keys.ENTER)
-            time.sleep(3)
-            current_msgs = driver.find_elements(By.CLASS_NAME, "top-center")
-            for index,msg in enumerate(current_msgs):
-                if msg not in init_msgs and index > len(init_msgs):
-                    print("text:", msg.text)
+        # input_element = driver.find_element(By.CLASS_NAME, 'textarea')
+        # if(input_element):
+        #     init_msgs = driver.find_elements(By.CLASS_NAME, "top-center")
+        #     input_element.send_keys(last_msg, Keys.ENTER)
+        #     time.sleep(3)
+        #     current_msgs = driver.find_elements(By.CLASS_NAME, "top-center")
+        #     for index,msg in enumerate(current_msgs):
+        #         if msg not in init_msgs and index > len(init_msgs):
+        #             print("text:", msg.text)
 
         # # 判断关键字
-        # msg = df.apply(lambda x: x['回复内容'] if x['关键词'] in last_msg else None, axis=1)
+        msg = df.apply(lambda x: x['回复内容'] if x['关键词'] in last_msg else None, axis=1)
         # # 数据筛选，移除空数据
-        # msg.dropna(axis=0, how='any', inplace=True)
+        msg.dropna(axis=0, how='any', inplace=True)
         # # 做成列表
-        # ar = np.array(msg).tolist()
+        ar = np.array(msg).tolist()
         # # 能够匹配到数据时
-        # if ar:
-        #     # 将数据输入
-        #     # 替换换行符号
-        #     wx.SendKeys(ar[0]+'{Enter}', waitTime=1)
-        #     # 通过消息匹配检索会话栏的联系人
-        #     wx.TextControl(SubName=ar[0][:5]).RightClick()
-        # # 没有匹配到数据时
-        # else:
-            # pass
+        print(f"test: {ar[0][:5]}")
+        if ar:
+            # 将数据输入
+            # 替换换行符号
+            # wx.SendKeys(ar[0]+'{Enter}', waitTime=1)
+            # 通过消息匹配检索会话栏的联系人
+            wx.TextControl(SubName="yto-test").RightClick()
+        # 没有匹配到数据时
+        else:
+            pass
 
     time.sleep(10)
